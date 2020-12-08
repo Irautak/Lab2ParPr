8151bb2eb5e353ac0f6d599aeb5f93dcb8ddbf18
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-08 07:09:29 +0300

    Scripted auto-commit on change (2020-12-08 07:09:29) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -2,8 +2,30 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Text> {
     @Override
-    protected void reduce(JoinWritableComparable key, Iterable<Text> vals, Context context) throws IOException
+    protected void reduce(JoinWritableComparable key, Iterable<Text> vals, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iterator = vals.iterator();
+        Text airportName = new Text("Airport name: " + iterator.next().toString());
+        if (iterator.hasNext()) {
+            int i =0;
+            float cur;
+            float min = 0.00f;
+            float max = 0.00f;
+            float avg = 0.00f;
+            while (iterator.hasNext()) {
+                cur = Float.parseFloat(iterator.next().toString());
+                if (cur < min || i == 0) min = cur;
+                else if (cur > max) max = cur;
+                avg += cur;
+                i++;
+            }
+            avg /= i;
+            Text stats = new Text("Min: " + min +
+                                    "Avg: " + avg +
+                                    "Max: " + max)
+        }
+    }
 }
