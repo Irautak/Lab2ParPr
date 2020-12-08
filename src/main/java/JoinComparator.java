7cd3550f2944126cca86afede71946ed2b0fe75c
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-08 06:59:38 +0300

    Scripted auto-commit on change (2020-12-08 06:59:38) by gitwatch.sh

--- src/main/java/JoinComparator.java
+++ src/main/java/JoinComparator.java
@@ -1,2 +1,14 @@
-public class JoinComparator {
+import org.apache.hadoop.io.WritableComparator;
+
+public class JoinComparator extends WritableComparator {
+    protected JoinComparator(){
+        super(JoinWritableComparable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparator a, WritableComparator b) {
+        JoinWritableComparable that = (JoinWritableComparable)a;
+        JoinWritableComparable other = (JoinWritableComparable)b;
+        return that.compareTo(other);
+    }
 }
