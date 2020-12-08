f9784d6f144ea0b4f4b464d42278c93c3eb0d497
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-08 07:04:42 +0300

    Scripted auto-commit on change (2020-12-08 07:04:42) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,2 +1,9 @@
-public class JoinReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Text> {
+    @Override
+    protected void reduce(JoinWritableComparable key, Iterable<Text> vals, Context context) throws IOException
 }
