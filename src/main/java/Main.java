6cd5a56be49b95f8599317b1811d6b9819845fa7
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-08 07:24:25 +0300

    Scripted auto-commit on change (2020-12-08 07:24:25) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,29 @@
-public class Main {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.Job;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Job job = Job.getInstance();
+        job.setJarByClass(Main.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setReducerClass(JoinReducer.class);
+        job.setPartitionerClass(JoinPartitioner.class);
+        job.setGroupingComparatorClass(JoinComparator.class);
+        job.setMapOutputKeyClass(JoinWritableComparable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setNumReduceTasks(4);
+
+    }
 }
