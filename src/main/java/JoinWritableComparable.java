d2d8aefb97e0d711c7b18be2c58f67964eb296f6
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-08 06:50:54 +0300

    Scripted auto-commit on change (2020-12-08 06:50:54) by gitwatch.sh

--- src/main/java/JoinWritableComparable.java
+++ src/main/java/JoinWritableComparable.java
@@ -1,2 +1,29 @@
 public class JoinWritableComparable {
+    private int airportID;
+    private int indicator;
+
+    public JoinWritableComparable() {}
+
+    public JoinWritableComparable(int airportID, int indicator) {
+        this.airportID = airportID;
+        this.indicator = indicator;
+    }
+
+    public void setAirportID(int airportID) {
+        this.airportID = airportID;
+    }
+
+    public void setIndicator(int indicator) {
+        this.indicator = indicator;
+    }
+
+    public int getAirportID() {
+        return airportID;
+    }
+
+    public int getIndicator() {
+        return indicator;
+    }
+
+    
 }
